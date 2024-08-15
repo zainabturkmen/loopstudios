7d4f88af605fde5d21fec13a980318a4a4b41bf4
zainabturkmen <zainabturkmen7@gmail.com>
2024-08-15 16:45:51 +0430

    init

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
+const btn = document.getElementById("menu-btn");
+const mneu = document.getElementById("menu");
 
-// const btn = document.getElementById('menu-btn')
-// const menu = document.getElementById('menu')
+btn.addEventListener("click", navToggle);
 
-// btn.addEventListener('click', navToggle)
+function navToggle() {
+  btn.classList.toggle("open");
+  mneu.classList.toggle("flex");
+  mneu.classList.toggle("hidden");
+}
+
+// const btn = document.getElementById("menu-btn");
+// const menu = document.getElementById("menu");
+
+// btn.addEventListener("click", navToggle);
 
 // function navToggle() {
-//   btn.classList.toggle('open')
-//   menu.classList.toggle('flex')
-//   menu.classList.toggle('hidden')
+//   btn.classList.toggle("open");
+//   menu.classList.toggle("flex");
+//   menu.classList.toggle("hidden");
 // }
